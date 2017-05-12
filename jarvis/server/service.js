3850254e6e9e4840c19d7c4bf5e7096ae09881c9
Aaron <pratt.aaron.m@gmail.com>
2017-05-12 11:02:42 +0300

    registry subscription for time service

--- jarvis/server/service.js
+++ jarvis/server/service.js
@@ -1,7 +1,19 @@
-'use strict'
+'use strict';
 
 const express = require('express')
 const service = express()
+
+// service registry
+service.put('/service/:intent/:port', (req, res, next) => {
+	const serviceIntent = req.params.intent
+	const servicePort = req.params.port
+
+	// for simplicity, we assume no proxy servers in path
+	const serviceIp = req.connection.remoteAddress.includes('::')
+		? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress
+
+	res.json({ result: `${serviceIntent} at ${serviceIp}:${servicePort}` })
+})
 
 
 
